910e0e3bffafc52d7b68efe03e99d3a955d8e921
S. Chris Colbert <sccolbert@gmail.com>
2015-09-03 12:16:29 -0400

    update tests

--- test/src/index.ts
+++ test/src/index.ts
@@ -10,140 +10,159 @@
 import expect = require('expect.js');
 
 import {
-  overrideCursor, hitTest, boxSizing, sizeLimits
-} from "../../lib/index";
+  OVERRIDE_CURSOR_CLASS, boxSizing, hitTest, overrideCursor, sizeLimits
+} from '../../lib/index';
+
+import './index.css';
 
 
 describe('phosphor-domutil', () => {
 
-  /**
-   * overrideCursor section
-   */
-  describe('overrideCursor', () => {
+  describe('OVERRIDE_CURSOR_CLASS', () => {
 
-    it('should change to wait', () => {
-      var delegate = overrideCursor('wait');
-      expect(document.body.style.cursor).to.be('wait');
+    it('should equal `p-mod-override-cursor`', () => {
+      expect(OVERRIDE_CURSOR_CLASS).to.be('p-mod-override-cursor');
     });
 
-    it('should revert when delegate is disposed', () => {
-      var delegate = overrideCursor('wait');
-      expect(document.body.style.cursor).to.be('wait');
-
-      delegate.dispose();
-      expect(document.body.style.cursor).to.be('');
-    })
   });
 
-  /**
-   * hitTest section
-   */
-  describe('hitTest', () => {
+  describe('overrideCursor()', () => {
 
-    it('should return true when inside, false when outside', () => {
-      var obj = document.createElement('img');
-      obj.style.position = 'absolute';
-      obj.style.left = '0';
-      obj.style.top = '0';
-      obj.width = 100;
-      obj.height = 100;
-      document.body.appendChild(obj);
+    it('should update the body `cursor` style', () => {
+      expect(document.body.style.cursor).to.be('');
+      var override = overrideCursor('wait');
+      expect(document.body.style.cursor).to.be('wait');
+      override.dispose();
+    });
 
-      var inside = hitTest(obj, 50, 50);
-      expect(inside).to.be(true);
+    it('should add the `p-mod-override-cursor` class to the body', () => {
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(false);
+      var override = overrideCursor('wait');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      override.dispose();
+    });
 
-      var outside = hitTest(obj, 150, 150);
-      expect(outside).to.be(false);
+    it('should clear the override when disposed', () => {
+      expect(document.body.style.cursor).to.be('');
+      var override = overrideCursor('wait');
+      expect(document.body.style.cursor).to.be('wait');
+      override.dispose();
+      expect(document.body.style.cursor).to.be('');
+    });
+
+    it('should remove the `p-mod-override-cursor` class when disposed', () => {
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(false);
+      var override = overrideCursor('wait');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      override.dispose();
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(false);
+    });
+
+    it('should respect the most recent override', () => {
+      expect(document.body.style.cursor).to.be('');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(false);
+      var one = overrideCursor('wait');
+      expect(document.body.style.cursor).to.be('wait');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      var two = overrideCursor('default');
+      expect(document.body.style.cursor).to.be('default');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      var three = overrideCursor('cell')
+      expect(document.body.style.cursor).to.be('cell');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      two.dispose();
+      expect(document.body.style.cursor).to.be('cell');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      one.dispose();
+      expect(document.body.style.cursor).to.be('cell');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(true);
+      three.dispose();
+      expect(document.body.style.cursor).to.be('');
+      expect(document.body.classList.contains('p-mod-override-cursor')).to.be(false);
+    });
+
+    it('should override the computed cursor for a node', () => {
+      var div = document.createElement('div');
+      div.className = 'cell-cursor';
+      document.body.appendChild(div);
+      expect(window.getComputedStyle(div).cursor).to.be('cell');
+      var override = overrideCursor('wait');
+      expect(window.getComputedStyle(div).cursor).to.be('wait');
+      override.dispose();
+      expect(window.getComputedStyle(div).cursor).to.be('cell');
+      document.body.removeChild(div);
+    });
+
+  });
+
+  describe('hitTest()', () => {
+
+    it('should return `true` when point is inside the node', () => {
+      var div = document.createElement('div');
+      div.className = 'hit-test';
+      document.body.appendChild(div);
+      expect(hitTest(div, 50, 50)).to.be(true);
+      document.body.removeChild(div);
+    });
+
+    it('should return `false` when point is outside the node', () => {
+      var div = document.createElement('div');
+      div.className = 'hit-test';
+      document.body.appendChild(div);
+      expect(hitTest(div, 150, 150)).to.be(false);
+      document.body.removeChild(div);
     });
 
     it('should use closed intervals for left and top only', () => {
-      var obj = document.createElement('img');
-      obj.style.position = 'absolute';
-      obj.style.left = '0';
-      obj.style.top = '0';
-      obj.width = 100;
-      obj.height = 100;
-      document.body.appendChild(obj);
-
-      var top_left_corner = hitTest(obj, 0, 0);
-      expect(top_left_corner).to.be(true);
-
-      var top_right_corner = hitTest(obj, 100, 0);
-      expect(top_right_corner).to.be(false);
-
-      var inside_top_right = hitTest(obj, 99, 0);
-      expect(inside_top_right).to.be(true);
-
-      var bottom_left_corner = hitTest(obj, 0, 100);
-      expect(bottom_left_corner).to.be(false);
-
-      var inside_bottom_left = hitTest(obj, 0, 99);
-      expect(inside_bottom_left).to.be(true);
-
-      var bottom_right_corner = hitTest(obj, 100, 100);
-      expect(bottom_right_corner).to.be(false);
-
-      var inside_bottom_right = hitTest(obj, 99, 99);
-      expect(inside_bottom_right).to.be(true);
+      var div = document.createElement('div');
+      div.className = 'hit-test';
+      document.body.appendChild(div);
+      expect(hitTest(div, 0, 0)).to.be(true);
+      expect(hitTest(div, 100, 0)).to.be(false);
+      expect(hitTest(div, 99, 0)).to.be(true);
+      expect(hitTest(div, 0, 100)).to.be(false);
+      expect(hitTest(div, 0, 99)).to.be(true);
+      expect(hitTest(div, 100, 100)).to.be(false);
+      expect(hitTest(div, 99, 99)).to.be(true);
+      document.body.removeChild(div);
     });
 
   });
 
-  /**
-   * boxSizing section
-   */
-  describe('boxSizing', () => {
+  describe('boxSizing()', () => {
 
     it('should return a box sizing with correct parameters', () => {
-      var obj = document.createElement('img');
-      obj.style.position = 'absolute';
-      obj.style.left = '0';
-      obj.style.top = '0';
-      obj.style.borderTop = "solid 10px black";
-      obj.style.borderLeft = "solid 10px black";
-      obj.style.paddingRight = '15px';
-      obj.style.paddingBottom = '15px';
-      obj.width = 100;
-      obj.height = 100;
-      document.body.appendChild(obj);
-
-      var sizing = boxSizing(obj);
-
+      var div = document.createElement('div');
+      div.className = 'box-sizing';
+      document.body.appendChild(div);
+      var sizing = boxSizing(div);
       expect(sizing.borderTop).to.be(10);
-      expect(sizing.borderLeft).to.be(10);
+      expect(sizing.borderLeft).to.be(15);
       expect(sizing.borderRight).to.be(0);
       expect(sizing.borderBottom).to.be(0);
-
-      expect(sizing.paddingTop).to.be(0);
-      expect(sizing.paddingLeft).to.be(0);
-      expect(sizing.paddingRight).to.be(15);
-      expect(sizing.paddingBottom).to.be(15);
+      expect(sizing.paddingTop).to.be(15);
+      expect(sizing.paddingLeft).to.be(12);
+      expect(sizing.paddingRight).to.be(8);
+      expect(sizing.paddingBottom).to.be(9);
+      expect(sizing.verticalSum).to.be(34);
+      expect(sizing.horizontalSum).to.be(35);
+      document.body.removeChild(div);
     });
 
   });
 
-  /**
-   * sizeLimits section
-   */
-  describe('sizeLimits', () => {
+  describe('sizeLimits()', () => {
 
     it('should return a size limits object with correct parameters', () => {
-      var obj = document.createElement('img');
-      obj.style.position = 'absolute';
-      obj.style.minWidth = '90px';
-      obj.style.minHeight = '90px';
-      obj.style.maxWidth = '120px';
-      obj.style.maxHeight = '120px';
-      obj.height = 100;
-      obj.width = 100;
-      document.body.appendChild(obj);
-
-      var limits = sizeLimits(obj);
-
+      var div = document.createElement('div');
+      div.className = 'size-limits';
+      document.body.appendChild(div);
+      var limits = sizeLimits(div);
       expect(limits.minWidth).to.be(90);
-      expect(limits.minHeight).to.be(90);
-      expect(limits.maxWidth).to.be(120);
-      expect(limits.maxHeight).to.be(120);
+      expect(limits.minHeight).to.be(95);
+      expect(limits.maxWidth).to.be(100);
+      expect(limits.maxHeight).to.be(105);
+      document.body.removeChild(div);
     });
 
   });
