2fe91534079eeee605e52ff24c3eab9975d352a3
S. Chris Colbert <sccolbert@gmail.com>
2015-09-03 19:01:46 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-domutil',
-  main: 'phosphor-domutil/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-domutil.d.ts',
-});
